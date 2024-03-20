6cb7c784732004820b77744df631dc628acf91c1
marcellryan <marcellinusryan12@gmail.com>
2024-03-20 09:45:44 +0700

    Pertemuan 2

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,23 @@ class App
 {
     public function __construct()
     {
-        echo 'Berhasil masuk ke class utama App !';
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL()
+    {
+        if (isset($_GET['url'])) {
+            //→ Ambil value urlnya
+            $url =$_GET['url'];
+            //→ Buang tanda '/' di akhir url
+            $url = rtrim($url,'/');
+            //→ Filter url dari karater2 aneh
+            $url = filter_var($url, FILTER_SANITIZE_URL );
+            //→ Pecah url berdasarkan tanda '/' sebagai pemisah
+            $url = explode('/', $url);
+
+            return $url;
+        }
     }
 }
